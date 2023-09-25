c1f1b90b93c6a73f3e1ff5d14976eaed551af3a3
Alexey Krasnoshchekov <alexburnitdown@gmail.com>
2023-09-25 11:17:02 +0300

    fixes 6

--- src/api/win/index.js
+++ src/api/win/index.js
@@ -1,15 +1,15 @@
 export const getWinData = async (date) => {
   const winDataMongo = await fetch(
-    // `https://tipster-server.vercel.app/win/get/?date=${date}`
-    `http://localhost:8000/win/get/?date=${date}`
+    `https://tipster-server.vercel.app/win/get/?date=${date}`
+    // `http://localhost:8000/win/get/?date=${date}`
   );
   const winDataMongoJson = await winDataMongo.json();
   return winDataMongoJson;
 };
 
 export const saveWinData = async (data) => {
-  // const resp = await fetch(`https://tipster-server.vercel.app/win/save`, {
-  const resp = await fetch(`http://localhost:8000/win/save`, {
+  const resp = await fetch(`https://tipster-server.vercel.app/win/save`, {
+  // const resp = await fetch(`http://localhost:8000/win/save`, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
